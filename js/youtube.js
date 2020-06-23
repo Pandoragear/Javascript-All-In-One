16ac34893a2ff674b2d3182bc35c4c871077034e
Andy Dang <andy.dang.it@gmail.com>
2020-06-22 21:29:30 -0500

    Manipulating HTML with JS

--- js/youtube.js
+++ js/youtube.js
@@ -1,1 +1,4 @@
-alert("hello world");
+document.getElementById("button").onclick = function(){
+    document.getElementById("confirm").innerHTML ="Order placed. Check email for confrimation";
+    document.getElementById("button").style.display = "none";
+}
