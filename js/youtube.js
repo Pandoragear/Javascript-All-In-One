5f5076079d064c5c6bcf77977cfb3274afb8698a
Andy Dang <andy.dang.it@gmail.com>
2020-06-22 22:13:16 -0500

    BAsic, Input, Output, Comments

--- js/youtube.js
+++ js/youtube.js
@@ -1,4 +1,16 @@
-document.getElementById("button").onclick = function(){
-    document.getElementById("confirm").innerHTML ="Order placed. Check email for confrimation";
-    document.getElementById("button").style.display = "none";
-}
+// document.getElementById("button").onclick = function(){
+//     document.getElementById("confirm").innerHTML ="Order placed. Check email for confrimation";
+//     document.getElementById("button").style.display = "none";
+// }
+
+
+let usernames = prompt();
+
+console.log(usernames);
+
+// This is really important 
+/*
+    This 
+    is a multiline
+    comment
+*/
