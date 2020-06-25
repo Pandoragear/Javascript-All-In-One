4f6a696b12671b56f85042001f8687dd736f80cd
Andy Dang <andy.dang.it@gmail.com>
2020-06-24 19:36:14 -0500

    IIFE, Scope, and  Window Object

--- js/youtube.js
+++ js/youtube.js
@@ -1,24 +1,4 @@
-// let username = prompt();
-// username = 5;
-// username = "hello";
-
-// console.log(username);
-
-// let age = 5;
-// console.log(age);
-// console.log(age);
-// console.log(age);
-
-
-let age = 50;
-//age = 50;
-
-let newAge = age;
-
-let message = "newAge = " + newAge;
-console.log(message);
-
-
-
-//declaraton
-//initialization
+(function(){
+    // IIFE - immediately invoked function expression
+    var age = 5;
+} )(); 
