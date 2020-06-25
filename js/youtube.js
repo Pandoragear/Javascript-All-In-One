d66d7e047c513cf66e56928de5b38e451df489a9
Andy Dang <andy.dang.it@gmail.com>
2020-06-24 22:38:05 -0500

    Convert Decimal Num to Binary, Octal, & Hexadecmal

--- js/youtube.js
+++ js/youtube.js
@@ -1,9 +1,14 @@
 {
-    let x = 5;
-    let y = "10";
-    console.log(x + y);
-    let yInt = Number.parseInt("10.932 is my fav number");
-    let yFloat = Number.parseFloat("10.9999 is my fav number");
-    console.log(x + yInt);
-    console.log(x + yFloat);
+    var input = Number(prompt("Put in a DECIMAL number"));
+
+//     console.log("Decimal    :", input);
+//     console.log("Binary     :", Number.parseInt(input, 2));
+//     console.log("Octal      :", Number.parseInt(input, 8));
+//     console.log("Hex        :", Number.parseInt(input, 16));
+
+    console.log(input + " in decimal to decimal:", input);
+    console.log(input,  " in decimal to binary:", input.toString(2));
+    console.log(input,  " in decimal to octal:", input.toString(8));
+    console.log(input,  " in decimal to hex:", input.toString(16));
+
 }
