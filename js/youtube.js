38e803c327ff5858346e454e34cfdf979be7af27
Andy Dang <andy.dang.it@gmail.com>
2020-06-24 20:10:13 -0500

    Block Scoping

--- js/youtube.js
+++ js/youtube.js
@@ -1,4 +1,12 @@
-(function(){
-    // IIFE - immediately invoked function expression
-    var age = 5;
-} )(); 
+{
+    //block 
+    let y = 10;
+    const x = 20;
+    {
+        let y = 6;
+        console.log(y);
+    }
+
+    console.log(y);
+    var z = 100;
+}
