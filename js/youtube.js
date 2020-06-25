6992395e30c26ab091c43343c3afadf1c4fbedd6
Andy Dang <andy.dang.it@gmail.com>
2020-06-24 22:29:18 -0500

    Parselnt & parseFloat Methods

--- js/youtube.js
+++ js/youtube.js
@@ -1,13 +1,9 @@
 {
-    let slicesOfPizza = 10;
-    // slicesOfPizza += 1;
-//     slicesOfPizza ++;
-//     ++slicesOfPizza;
-//     slicesOfPizza --;
-//     console.log(slicesOfPizza);
-
-let newPizza = slicesOfPizza++;
-console.log("slicesOfPizza:", slicesOfPizza);
-console.log("newPizza:", newPizza);
-
+    let x = 5;
+    let y = "10";
+    console.log(x + y);
+    let yInt = Number.parseInt("10.932 is my fav number");
+    let yFloat = Number.parseFloat("10.9999 is my fav number");
+    console.log(x + yInt);
+    console.log(x + yFloat);
 }
