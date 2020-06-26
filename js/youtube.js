d8f650f600d4f4c1e22d0233fea336c75ac801ee
Andy Dang <andy.dang.it@gmail.com>
2020-06-25 21:58:06 -0500

    Logical Operators

--- js/youtube.js
+++ js/youtube.js
@@ -1,7 +1,9 @@
 {
-  var age = prompt();
-  if(age > 12){
+  var age = prompt("age?");
+  var name = prompt("name?");
+  if(age < 13 && name === "Caleb" ){
     console.log("welcome!");
-
-  }
+  } else {
+    console.log("Go Away")
+  };
 }
