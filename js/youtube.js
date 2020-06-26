5e69666f329b6572a411958d50137fe8704f311a
Andy Dang <andy.dang.it@gmail.com>
2020-06-25 21:27:57 -0500

    If, Else, Else If

--- js/youtube.js
+++ js/youtube.js
@@ -1,34 +1,10 @@
 {
-  let position = {
-    x: 10, 
-    y: 20,
-    print: function (){
-      console.log(`X: ${this.x}, y: ${this.y}`);
-    },
-
-    myObject: { 
-      sweetProperty: "hello Friends"
-
-    }
-    
+  var name = prompt();
+  if(name === "Caleb"){
+    console.log("welcome!");
+  }else if( name === "Andy"){
+    console.log("Keep working hard");
+  } else {
+    console.log(" you are not welcome!");
   }
-
-  // let myPosition = position; 
-
-  // console.log(position);
-  // console.log(myPosition);
-
-  // myPosition.x = 15;
-
-  // console.log(position);
-  // console.log(myPosition);
-
-  function print (){
-    console.log(`X: ${this.x}, y: ${this.y}`);  //undefined
-  }
-
-  print();
-  position.print();
-  position.myObject.sweetProperty;
-
 }
