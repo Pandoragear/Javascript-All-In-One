40cd8b4e23c7d735a5c131fb04073eeb77a1a75e
Andy Dang <andy.dang.it@gmail.com>
2020-06-25 21:37:06 -0500

    Comparison Operators

--- js/youtube.js
+++ js/youtube.js
@@ -1,10 +1,7 @@
 {
-  var name = prompt();
-  if(name === "Caleb"){
+  var age = prompt();
+  if(age > 12){
     console.log("welcome!");
-  }else if( name === "Andy"){
-    console.log("Keep working hard");
-  } else {
-    console.log(" you are not welcome!");
+
   }
 }
