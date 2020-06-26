5a7a2ad67645475d44993bd822fc33bf0f83faa2
Andy Dang <andy.dang.it@gmail.com>
2020-06-25 22:30:47 -0500

    Ternary Operator

--- js/youtube.js
+++ js/youtube.js
@@ -1,7 +1,14 @@
 {
-  let name = prompt("What is your name");
-  if(name === "Andy")  { console.log("Hello"); }
+  let name = prompt("What is my name");
+  let points = name === "Andy" ? console.log("10") : console.log("0");
 
 
 
+
+  // if (name === "Caleb") {
+  //   points = 10; 
+  // } else {
+  //   point = 0;
+  // }
+  // console.log(points)
 }
