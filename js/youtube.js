4f7849090221f143a982ca29b1b5059059f70e61
Andy Dang <andy.dang.it@gmail.com>
2020-06-26 22:16:22 -0500

    Break & Continue

--- js/youtube.js
+++ js/youtube.js
@@ -4,12 +4,15 @@
   //   password = prompt("What is the passcode?");
   // }while(password.toLowerCase() !== "let me in");
 
-  let myString = "Search this string baby";
+  let myString = "cSearch this c string cc baby";
   let chartoSearch = "c";
   for (let i = 0; i <myString.length; i++) {
-    if(myString[i] === chartoSearch){
-      console.log(myString[i] + " is found at index " + i);
+    if( myString[i] === 'a' || myString[i] === 'e' || 
+        myString[i] === 'i' || myString[i] === 'o' || 
+        myString[i] === 'u'){
+      continue;
     }
+    console.log(myString[i]);
   }
 
 }
