64cc7d51ee696436c015c91611eba6fd60419216
Andy Dang <andy.dang.it@gmail.com>
2020-06-29 21:35:19 -0500

    Average of Array Values

--- js/youtube.js
+++ js/youtube.js
@@ -1,28 +1,21 @@
 {
-let grades = [12, 4, 4, 66, 7, 9, 33, 4, 6, 43, 43, 43];
+let grades = [15, 4, 4, 66, 7, 9, 33, 4, 6, 43, 43, 43];
 grades.length = 30;
 grades[34] = 40;
 
-  let largest = grades[0];
 
-  let found = false;
-  let serach = 43;
-
+  let count = 0;
+  let total = 0;
 
   for(let i = 0; i < grades.length; i++){
-    if (grades[i] > largest) {
-      largest = grades[i];
-
-      // found == true;
-      // console.log(grades[i] + " at index " + i);
-      // break;
-    }
+    if (grades [i] !== undefined) {
+      // legit value
+      count++;
+      total += grades[i];
   }
+}
 
-  console.log(largest);
-
-  // if(found){
-  //   //do something
-  // }
+let avg = total / count;
+console.log(avg);
 
 }
