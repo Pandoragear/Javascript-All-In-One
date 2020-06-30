bd6c2ce693263b3666cc75f970d2a6aaa818bd47
Andy Dang <andy.dang.it@gmail.com>
2020-06-29 20:48:54 -0500

    Intro to Multidimensional Arrays

--- js/youtube.js
+++ js/youtube.js
@@ -1,11 +1,14 @@
 {
-  let d = document.getElementById("destination");
-  for(let i = 0; i < 10; i++) {
-    for(let k = i; k >= 0; k--){
-        d.append(k + " ");
-    }
-    let br = document.createElement("br");
-    d.appendChild(br);
-  }
+  // let d = document.getElementById("destination");
+  // for(let i = 0; i < 10; i++) {
+  //   for(let k = i; k >= 0; k--){
+  //       d.append(k + " ");
+  //   }
+  //   let br = document.createElement("br");
+  //   d.appendChild(br);
+  // }
+
+  let grades = [[24, 3, 5], [100, 3, 5], [3, 5, 77]];
+  console.log(grades[1][2]);
 
 }
