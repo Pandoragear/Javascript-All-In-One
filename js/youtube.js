382d83f541fd28ca5bfc40cae3d1463c71ba6b82
Andy Dang <andy.dang.it@gmail.com>
2020-06-29 21:46:51 -0500

    Fill Array, User Input, indef Loop, Sentinel Value

--- js/youtube.js
+++ js/youtube.js
@@ -1,21 +1,14 @@
 {
-let grades = [15, 4, 4, 66, 7, 9, 33, 4, 6, 43, 43, 43];
-grades.length = 30;
-grades[34] = 40;
+  let grades = [];
 
+  while(true){
+    let input = prompt(" Add a grade");
+    if(input === "q" || input === null){
+      break;
+    }
 
-  let count = 0;
-  let total = 0;
-
-  for(let i = 0; i < grades.length; i++){
-    if (grades [i] !== undefined) {
-      // legit value
-      count++;
-      total += grades[i];
+    grades.push(Number(input));
+    console.log(grades);
   }
-}
-
-let avg = total / count;
-console.log(avg);
 
 }
