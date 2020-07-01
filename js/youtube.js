14851fae2de9b733b6649159be9f9e0a100191b2
Andy Dang <andy.dang.it@gmail.com>
2020-06-30 21:39:04 -0500

    Iterate Multidimensional Array with for & forEach

--- js/youtube.js
+++ js/youtube.js
@@ -1,14 +1,22 @@
 {
-  let grades = [12, 13, 32, 43, 42, 23];
-  grades.length = 30;
+  let grades = [
+    [12, 13, 32, 43, 42, 23],
+    [12, 43, 21, 12, 43, 12],
+    [43, 12, 43, 12, 45, 12]
+  ];
 
-  // for (let i = 0; i < grades.length; i++){
-  //   if(grades[i] !== undefined) 
-  //   console.log(grades[i]);
+  // for(let i = 0; i < grades.length; i++) {
+  //   for(let k = 0; k < grades[i].length; k++ ) {
+  //     console.log(grades[i] [k]);
+  //   }
+  //   console.log("------")
   // }
 
-  grades.forEach(function(element, i, array){
-    console.log(element, i, array);
+  grades.forEach(function(row){
+      row.forEach(function(col){
+        console.log(col);
+      });
+      console.log("----");
     });
 
 }
