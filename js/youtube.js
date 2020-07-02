43e0ed6153124253b78a9949e0e19a6ca7e985fb
Andy Dang <andy.dang.it@gmail.com>
2020-07-01 21:54:51 -0500

    Using Dates and Unix Timestamp in JS

--- js/youtube.js
+++ js/youtube.js
@@ -1,4 +1,30 @@
 {
-  var valentine = new Date();  // constructor
-  console.log(valentine);
+  // let myDate = new Date();
+  // let time = Date.now();
+  // console.log(time);
+
+  // let start = Date.now();
+
+  //////////////////////////////////
+
+  // let x = 0;
+  // for(let i = 0; i < 100000; i++){
+  //   x = x + i;
+
+  // }
+
+  // let end = Date.now();
+  // let total = end - start;
+  // console.log(`Time took ${total}`);
+  // console.log(x);
+
+////////////////////////////////////////
+
+  let before = new Date (2020, 10, 15);
+  let after = new Date(2020, 10, 20);
+
+  let oneDay = 1000 * 60 * 60 * 24;
+  let days = (after - before) / oneDay;
+  console.log(days);
+
 }
