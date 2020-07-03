07b1104e6d8898c6c8bc5de89e51be7291beffd2
Andy Dang <andy.dang.it@gmail.com>
2020-07-03 00:05:30 -0500

    Default Param, Rest Param, Implicit Parameter

--- js/youtube.js
+++ js/youtube.js
@@ -1,30 +1,30 @@
 {
-    pow.calculated  = {};
-    function pow(x, y) {
-        let stringVersion = x + "^" + y;
-        console.log("string veresion: " + stringVersion);
 
+    function pow (x, y, ...extra) {
+        console.log(this);
+        console.log(arguments);
+        console.log(extra);
+        // if(y == undefined) {y = 2}
 
-        if(stringVersion in pow.calculated){
-            console.log("found!");
-            return pow.calculated[stringVersion];
-        }
-
-        console.log("CALCULATING!");
-
-        let total = 1; 
-        for(let i = 0; i < y; i++) {
+        // y = typeof y === "undefined" ? 2 : y;
+        let total = 1;
+        for (let i = 0; i < y; i++) {
             total *= x;
         }
-        // pow.calculated.push(total);
-        pow.calculated[stringVersion] = total;
-        console.log(pow.calculated);
         return total;
     }
 
-    pow(3,3,);
-    pow(3,3,);
-    pow(10,4);
-    pow(10,3);
+    function largest(x, ...extra) {
+        let largest = x;
+        for(let i = 0; i < extra.length; i++) {
+            if(extra[i] > largest){
+                largest = extra[i];
+            }
+        }
+        return largest;
+    }
+
+    console.log(pow(3, 3, 4, 5, 6, 6));
+    
 
 }
