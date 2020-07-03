201b5986369a02b2aa485d9c5ee92240ecab9491
Andy Dang <andy.dang.it@gmail.com>
2020-07-02 22:27:24 -0500

    Function Declarations & Expression

--- js/youtube.js
+++ js/youtube.js
@@ -1,10 +1,13 @@
 {
-//  let myDate = Date.parse('12 Jan 195 00:15:54 GMT');
-//  let myDate = new Date(Date.UTC(2012, 11, 15, 10, 10, 10));
- let myDate = new Date(2012, 10, 12);
-//  myDate.getTimezoneOffset();
- console.log(myDate.getTimezoneOffset() / 60);
+let myFunc = function (x, y) {
+    let total = 1;
+    for (let i = 0; i < y; i++) {
+        total *=x;
+    }
+    return total;
+}
 
+console.log(myFunc(3,3))
 
 
 }
