4d9a19e08d93b06881dd24d781a4fd2baa1baf4e
Andy Dang <andy.dang.it@gmail.com>
2020-07-02 22:33:31 -0500

    Hoisting

--- js/youtube.js
+++ js/youtube.js
@@ -1,13 +1,24 @@
 {
-let myFunc = function (x, y) {
-    let total = 1;
-    for (let i = 0; i < y; i++) {
-        total *=x;
-    }
-    return total;
+// let myFunc = function (x, y) {
+//     let total = 1;
+//     for (let i = 0; i < y; i++) {
+//         total *=x;
+//     }
+//     return total;
+// }
+
+// console.log(myFunc(3,3))
+
+///////////////////////////
+
+// Hoisting
+
+// console.log(x);
+// var x = 10;
+
+doStuff();
+function doStuff() {
+    console.log("things"); // return things;
 }
-
-console.log(myFunc(3,3))
-
 
 }
