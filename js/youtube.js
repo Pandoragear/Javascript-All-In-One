dd706e629b5108afe3fdcf91abb0350b62e6a439
Andy Dang <andy.dang.it@gmail.com>
2020-07-03 22:31:46 -0500

    Call and Apply

--- js/youtube.js
+++ js/youtube.js
@@ -1,36 +1,13 @@
 {
-
-let me = {
-    name: "Caleb",
-    outputMe: function(){
-        console.log(this);
-        console.log(this.name);
-        
-    }
-};
-
-     function outputMe(){
-        'use strict';
-    console.log(this);
-
-}
-
-    function outputMeStrict(){
-        'use strict';
+    function doStuff(input, input2){
+        console.log(input, input2);
         console.log(this);
     }
 
-    function Person(){
-        console.log(this);
-        this.name = "Caleb";
-        console.log(this);
+    let args = [5, 10];
+    // doStuff.call("hello", 5, 10); // call 
+    doStuff.apply("hello", [5, 10]); // apply
 
-    }
 
-    me.outputMe();   //method
-    outputMe();     //function
-    outputMeStrict();  // function strict
-    let person  = new Person(); // ctor
-    console.log(person);
 
 }
