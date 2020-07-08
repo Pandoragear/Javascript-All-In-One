cb0cdbb445c0d2ea69de53b57e7e1cf4d3800bb0
Andy Dang <andy.dang.it@gmail.com>
2020-07-07 23:05:53 -0500

    Creating a Factory Function

--- js/youtube.js
+++ js/youtube.js
@@ -1,12 +1,32 @@
 {
+    //Constructor Function 
     function User(name, interest){
         this.name = name;
         this.interests = interest;
                 
     }
 
-    let me = new User("Caleb", ["cooking", "eating", "excercise" ]);
-    let you = new User("Camila" ["hotwheels"]);
+    //Factory Function 
+    function newUser(name, interest) {
+        let person = {
+            name: name,
+            interest: interest
+        };
+
+        return person; 
+    }
+
+    // Constructor Function Output 
+    // let me = new User("Caleb", ["cooking", "eating", "excercise" ]);
+    // let you = new User("Camila" ["hotwheels"]);
+    // console.log(me, you);
+    // me.membership = "Gold";
+    // console.log(me);
+
+
+    // Factory Function Output
+    let me =  newUser("Caleb", ["cooking", "eating", "excercise" ]);
+    let you = newUser("Camila" ["hotwheels"]);
     console.log(me, you);
     me.membership = "Gold";
     console.log(me);
