deb0f99dec3e3cc4b5a77b6418110c0296bea268
Andy Dang <andy.dang.it@gmail.com>
2020-07-08 23:36:56 -0500

    Check an Object for a Property Using in

--- js/youtube.js
+++ js/youtube.js
@@ -35,30 +35,10 @@
         console.log(e.sayHello());
     });
 
+    console.log("Name in teacher? ", "name" in teacher);
+
+    console.log("Name in teacher?", teacher.name !== undefined);
+
+    console.log(teacher);
 
 }
-
-// function doSomething(){
-//     return 1 + 1;
-// }
-
-// // doSomething -- object.constructor -- doSomething
-
-// let test = new doSomething();
-
-// if(doSomething.prototype === Object.getPrototypeOf(test)){
-//     console.log("match");
-// }
-
-
-function Taco(){
-    this.toppings = ["cheese"];
-}
-
-Taco.prototype.make = function(){
-    console.log("Making a taco...");
-};
-
-
-let myTaco = new Taco();
-let newTaco = new Taco();
