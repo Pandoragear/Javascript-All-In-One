c6556aeba6fe3a0229f6a2e8c56697417f282bdc
Andy Dang <andy.dang.it@gmail.com>
2020-07-08 22:35:01 -0500

    Prototype Methods for Constructor Function

--- js/youtube.js
+++ js/youtube.js
@@ -1,35 +1,22 @@
 {
-    //Constructor Function 
+
     function User(name, interest){
         this.name = name;
         this.interests = interest;
+
                 
     }
 
-    //Factory Function 
-    function newUser(name, interest) {
-        let person = {
-            name: name,
-            interest: interest
-        };
+    User.prototype.greet = function(){
+        console.log("my name is " + this.name, this.interests);
+    };
 
-        return person; 
-    }
-
-    // Constructor Function Output 
-    // let me = new User("Caleb", ["cooking", "eating", "excercise" ]);
-    // let you = new User("Camila" ["hotwheels"]);
-    // console.log(me, you);
-    // me.membership = "Gold";
-    // console.log(me);
-
-
-    // Factory Function Output
-    let me =  newUser("Caleb", ["cooking", "eating", "excercise" ]);
-    let you = newUser("Camila" ["hotwheels"]);
+    let me =  new User("Caleb", ["cooking", "eating", "excercise" ]);
+    let you = new User("Camila", ["hotwheels"]);
     console.log(me, you);
-    me.membership = "Gold";
-    console.log(me);
+
+    me.greet();
+    you.greet();
 
 
 }
