6b280cd6025678facfc50d8f0c61dc6f82de0edc
Andy Dang <andy.dang.it@gmail.com>
2020-07-08 23:02:23 -0500

    Instance Properties vs prototype properties

--- js/youtube.js
+++ js/youtube.js
@@ -1,23 +1,29 @@
 {
     let user = {
-        active: true
+        active: false,
+        sayHello: function(){
+            return this.name + " says hi!";
+        }
     };
 
     let student = {
+        name: "Peasant student",
         major: "English"
     };
 
 
     let teacher = {
+        name: "Caleb Curry",
         teaching: ["math", "science"]
     };
 
     Object.setPrototypeOf(student, user);
     Object.setPrototypeOf(teacher, user);
 
-    student.active = false;
-    console.log("teacher", teacher.active);
-    console.log("student", student.active);
+    student.active = true;
+
+    console.log("teacher", teacher.sayHello());
+    console.log("student", student.sayHello());
 
 
 
