c89e98fdbf03ffdcb4ee7c166bf4539f33340ed7
Andy Dang <andy.dang.it@gmail.com>
2020-07-08 22:50:25 -0500

    Override in Prototypal Inheritance

--- js/youtube.js
+++ js/youtube.js
@@ -3,13 +3,21 @@
         active: true
     };
 
+    let student = {
+        major: "English"
+    };
+
+
     let teacher = {
         teaching: ["math", "science"]
     };
 
+    Object.setPrototypeOf(student, user);
     Object.setPrototypeOf(teacher, user);
 
-    console.log(teacher);
+    student.active = false;
+    console.log("teacher", teacher.active);
+    console.log("student", student.active);
 
 
 
