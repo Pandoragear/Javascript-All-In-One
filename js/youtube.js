ddfc2645cb4e0ad840b552e5ed7e8bff8d4c94cd
Andy Dang <andy.dang.it@gmail.com>
2020-09-03 23:11:51 -0500

    Setting Prototypes with Constructors

--- js/youtube.js
+++ js/youtube.js
@@ -12,19 +12,23 @@
         this.major = major;  
     }
 
-    function Teacher() {
-        this.name = "Caleb Curry";
-        this.teaching = ["math", "science"];
+    Student.prototype = new User();
+
+    function Teacher(name, teaching) {
+        this.name = name;
+        this.teaching = teaching;
     }
 
 
+    Teacher.prototype = new User();
+    Teacher.prototype.sayHello = function(){
+        return "Teacher says hi!";
+    }
 
     let student = new Student("peasant student", "English");
     let teacher = new Teacher("Caleb Curry", ["math", "science"]);
 
     console.log(student, teacher);
-
-
 
 
 
